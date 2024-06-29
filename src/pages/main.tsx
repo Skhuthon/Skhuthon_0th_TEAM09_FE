3b422d59334d44222bf957983fadcc190af40b42
Hueeng <zzxx373014@gmail.com>
2024-06-29 19:45:28 +0900

    #13 상단부분 이미지 추가 및 세팅

---
+++ src/pages/main.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { css } from "@emotion/css";
+import Image from "next/image";
+
+export default function Main() {
+  return (
+    <div className={container}>
+      <div className={imgContainer}>
+        <Image
+          src="/main/mainlogo.png"
+          width={1920}
+          height={300}
+          alt="메인 이미지"
+          className={imageStyle}
+        />
+        <div className={overlay}>
+          <h1 className={overlayText}>제품소개</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const container = css`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  position: relative;
+  margin: 0;
+`;
+
+const imgContainer = css`
+  height: 20%;
+  overflow: hidden;
+  position: relative;
+`;
+
+const imageStyle = css`
+  margin: -130px 0 0 0;
+  width: 100%;
+  height: auto;
+`;
+
+const overlay = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlayText = css`
+  color: white;
+  font-size: 3rem;
+  font-weight: bold;
+`;
